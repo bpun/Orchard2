772e61ce1e01fb717aa2f5bbdd8c2f9ccb8ec399
Nicholas Mayne <Jetski5822@gmail.com>
2017-01-04 13:17:58 +0800

    Starting to introduct nancy and fixing the leaking of MVC in to core componants

--- src/Orchard.ContentManagement/DefaultContentManager.cs
+++ src/Orchard.ContentManagement/DefaultContentManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Modules;
 using Microsoft.Extensions.Logging;
 using Orchard.ContentManagement.Handlers;
 using Orchard.ContentManagement.Metadata.Builders;
